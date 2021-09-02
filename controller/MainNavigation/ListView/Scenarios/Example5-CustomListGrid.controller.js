3ebc873658b8c4486152f34574cf0a5e122ffa30
Konstantinos Drachtidis <konstantinos.drachtidis@sap.com>
2021-09-02 14:42:43 +0200

    Folder structure optimization

---
+++ controller/MainNavigation/ListView/Scenarios/Example5-CustomListGrid.controller.js
@@ -0,0 +1,36 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
+    "use strict";
+
+    var Controller = Controller.extend("DemoUXapp.controller.MainNavigation.ListView.Scenarios.Example5-CustomListGrid", {
+        onInit: function () {
+            var sURL = "/";
+            var amodel = new sap.ui.model.odata.ODataModel(sURL, true);
+            var oModel = new sap.ui.model.json.JSONModel();
+            amodel.read("/Scenarios", {
+                success: function (oRetrievedResult) {
+                    oModel.setData(oRetrievedResult);
+                },
+                error: function (oError) { alert(`Error ${oError}`); }
+            });
+            this.getView().setModel(oModel);
+        },
+        onPress: function (oEvent) {
+            var oItem, oCtx, oAttr;
+            oItem = oEvent.getSource();
+            oCtx = oItem.getBindingContext();
+            oAttr = oCtx.getProperty("ScenarioNo");
+            console.log("ScenarioNo: " + oAttr);
+            this.getRouter().navTo("sap.uxap.ObjectPageSubSection/sap.f.GridList/sap.m.CustomListItem", {
+                scenarioId: oAttr
+            });
+        },
+        getRouter: function () {
+            return sap.ui.core.UIComponent.getRouterFor(this);
+        }
+    });
+
+    return Controller;
+}
+);
